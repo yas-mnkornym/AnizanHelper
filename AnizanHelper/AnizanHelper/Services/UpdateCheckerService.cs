f280fb667b2a59aecb089bfea1425b12b7881648
manayome <manayome@dummy.com>
2019-04-27 15:10:47 +0900

    アップデート機能を実用化できるよう

--- AnizanHelper/AnizanHelper/Services/UpdateCheckerService.cs
+++ AnizanHelper/AnizanHelper/Services/UpdateCheckerService.cs
@@ -15,10 +15,11 @@ namespace AnizanHelper.Services
 	[Service]
 	class UpdateCheckerService : ReactiveServiceBase
 	{
-		static TimeSpan CheckInterval { get; } = TimeSpan.FromMinutes(10);
+		static TimeSpan CheckInterval { get; } = TimeSpan.FromSeconds(10);
 
 		IUpdateManager UpdateManager { get; }
 		Settings Settings { get; }
+		Version IgnoreVersion { get; set; } = AppInfo.Current.Version;
 
 		public UpdateCheckerService(
 			IUpdateManager updateManager,
@@ -31,12 +32,17 @@ namespace AnizanHelper.Services
 		public async Task CheckForUpdateAndShowDialogIfAvailableAsync()
 		{
 			await UpdateManager.CheckForUpdateAsync();
+			var updateInfo = UpdateManager.UpdateInfo;
+
+			if (updateInfo.Version <= IgnoreVersion)
+			{
+				return;
+			}
 
 			var sb = new StringBuilder();
 			sb.AppendLine("アニソンDBぱーさーのアップデートがあります。");
 			sb.AppendLine("Webサイトを開きますか？");
 
-			var updateInfo = UpdateManager.UpdateInfo;
 			if (updateInfo != null) {
 				sb.AppendLine();
 				sb.AppendLine("最新バージョン: {0}", updateInfo.Version);
@@ -44,9 +50,17 @@ namespace AnizanHelper.Services
 
 			sb.AppendLine();
 			sb.AppendLine("※ アップデートをダウンロードしてファイルを上書きしてください。");
+			sb.AppendLine();
+			sb.AppendLine("※ キャンセル を選択すると、次回起動時までこのバージョンは通知されなくなります。");
 
-			if(MessageBox.Show(sb.ToString(), "アップデート確認", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.OK) == MessageBoxResult.Yes){
+			var ret = MessageBox.Show(sb.ToString(), "アップデート確認", MessageBoxButton.YesNoCancel, MessageBoxImage.Question, MessageBoxResult.OK);
+			if (ret == MessageBoxResult.Yes)
+			{
 				Process.Start(Constants.AnizanHelperWebSiteUrl);
+			}
+			else if (ret == MessageBoxResult.Cancel)
+			{
+				this.IgnoreVersion = updateInfo.Version;
 			}
 		}
 
